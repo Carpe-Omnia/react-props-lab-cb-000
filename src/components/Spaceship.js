f8b8a1c003dd150ab4ccde1e5d18e508ef389b0a
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-03-06 22:25:25 +0000

    Automatically backed up by Learn

--- src/components/Spaceship.js
+++ src/components/Spaceship.js
@@ -4,6 +4,13 @@ export default class Spaceship extends React.Component {
   super(props) ;
 
   render() {
-
+    return (
+      <div>
+        <p> {this.props.name} </p>
+        <p> {this.props.speed} </p>
+        <p> has rockets: {this.props.hasRockets} </p>
+        <p> colors: {this.props.colors} </p>
+      </div>
+    )
   }
 }
